fde1dd7c36ec059841ad0760f50665ab659ebd36
newest_repo <jbrink0925@gmail.com>
2020-11-02 23:44:35 -0500

    Bite 213 first test passed, maybe make changes to regex by putting regex in groups?

--- Bite_213.py
+++ Bite_213.py
@@ -1,3 +1,6 @@
+import re
+
+
 bite_15_en = '''<p>Iterate over the given <code>names</code> and <code>countries list</code>s, <strong>printing</strong> them prepending the number of the loop (starting at 1). Here is the output you need to deliver:<pre>
 1. Julian     Australia
 2. Bob        Spain
@@ -15,3 +18,37 @@ bite_15_it_fixed = '''<p>Iterare i <code>names</code> e le <code>countries list<
 5. Martin     USA
 6. Rodolfo    Mexico
 </pre></p><p>Si noti che la seconda colonna dovrebbe avere una larghezza fissa di 11 caratteri, quindi tra <i>Julian</i> e <i>Australia</i> ci sono 5 spazi, tra <i>Bob</i> e <i>Spagna</i> , ci sono 8. Questa colonna dovrebbe anche essere allineata a sinistra. </p><p>Idealmente si utilizza solo uno for loop, ma questo non è un requisito. </p><p>Buona fortuna e mantenere la calma e codice in Python! </p>'''
+
+
+
+
+
+def fix_translation(org_text=bite_15_en, trans_text=bite_15_it):
+    """Receives original English text as well as text returned by translator.
+       Parse trans_text restoring the original (English) code (wrapped inside
+       code and pre tags) into it. Return the fixed translation str
+    """
+    pre_corrections = re.findall(r'<pre>[A-Za-z0-9\s\.]+</pre>',org_text)
+    pre_corrections = ' '.join(pre_corrections)
+    code_corrections = re.findall(r'<code>[a-zA-Z0-9\s]+</code>',org_text)
+    t_text = re.sub(r'<pre>\s[a-zA-Z0-9\.\s]+[\w\W]+?</pre>',pre_corrections,trans_text)
+    trans_text_code_subs = re.findall(r'<code>[a-zA-Z0-9\s]+</code>',t_text)
+
+    i = 0
+    for item in trans_text_code_subs:
+        if item in t_text:
+            t_text = t_text.replace(item, code_corrections[i])
+        i += 1
+    
+    return t_text == bite_15_it_fixed
+
+    #print('t_text:\n\n\n')
+    #print(t_text)
+    #print()
+    #print('expected:\n\n')
+    #print(bite_15_it_fixed)
+    
+
+
+
+print(fix_translation())
