e34ac9a9783ce6d928c5c2246b1e4c26f33f03b6
newest_repo <jbrink0925@gmail.com>
2020-11-05 11:00:02 -0500

    closer to passing test #2; updating progress

--- Bite_224.py
+++ Bite_224.py
@@ -17,8 +17,9 @@ dot (.) should not end this sentence, the next one should. Have fun!
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    sentences = re.findall(r'[A-Za-z0-9\s,]+[\.\?!]{1}',text)
+    #sentences = re.findall(r'([A-Z][^\.!?]*[\.!?])',text)
+    sentences = re.findall(r'[A-Za-z0-9\s,()]+[\.\?!]{1}',text)
     return [sentence.replace('\n',' ').strip() for sentence in sentences]
     
 
-print(get_sentences(TEXT)) 
+print(get_sentences(TEXT_WITH_DOTS)) 
