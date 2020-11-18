8aeb7975ed5a713a01ad952186c3d5fa3f5317e9
J B <jbrink0925@gmail.com>
2020-11-18 12:36:39 -0500

    Bite 31 matmul special method working

---
+++ Bite_31.py
@@ -0,0 +1,44 @@
+class Matrix(object):
+
+    def __init__(self, values):
+        self.values = values
+
+    def __repr__(self):
+        return f'<Matrix values="{self.values}">'
+    
+    def __matmul__(self, other):
+        total = 0
+        left = self.values
+        right = other.values
+        
+        larger = None
+        smaller = None
+
+        if len(left) >= len(right):
+            larger = left
+            smaller = right
+        else:
+            larger = right
+            smaller = left
+        
+        
+        for i in range(len(larger)):
+            for j in range(len(larger[i])):
+                if larger[i][j] == larger[0][0]:
+                    total += (larger[i][j] * smaller[i][j]) + (larger[i][j] * smaller[i][j+1])
+                    total += (larger[i+1][j] * smaller[i][j]) + (larger[i+1][j] * smaller[i][j+1])
+                    total += (larger[i][j+1] * smaller[i+1][j]) + (larger[i][j+1] * smaller[i+1][j+1])
+                    total += (larger[i+1][j+1] * smaller[i+1][j]) + (larger[i+1][j+1] * smaller[i+1][j+1])
+            break
+                
+        return total
+
+
+
+
+x = [[1,2],[3,4]]
+y = [[11,12],[13,14]]
+
+m = Matrix(x)
+m2 = Matrix(y)
+print(m @ m2)
