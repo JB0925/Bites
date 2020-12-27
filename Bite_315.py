bfcb752c4db0ec0b3ba9d0dbcc245e79de7553f5
J B <jbrink0925@gmail.com>
2020-12-26 23:58:44 -0500

    updated Bite 315 and .gitignore

--- Bite_315.py
+++ Bite_315.py
@@ -15,19 +15,19 @@ def sum_even_numbers(numbers: List[float]) -> float:
        (e.g. one of the numbers is a str), catch
        the exception log it, then reraise it.
     """
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, filename='example.txt')
     try:
         output = sum([n for n in numbers if n % 2 == 0])
-        logger.info(f'Input: {numbers} -> output: {output}')
+        logger.info(f'Input: {numbers} -> output: {output}\n')
         return output
-        
+
     except TypeError as t:
-        logger.exception(f'Bad inputs: {numbers}')
+        logger.exception(f'Bad inputs: {numbers}\n')
         raise 
         
     
     
     
 
-print(sum_even_numbers([1,2,3,'a',5,6]))
+print(sum_even_numbers([1,2,3,4,5,6]))
 
