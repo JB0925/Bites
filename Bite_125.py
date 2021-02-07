18c4dc33df44799946a4518560352209dc2ca986
J B <jbrink0925@gmail.com>
2021-02-07 13:31:42 -0500

    Finished Bite 125

---
+++ Bite_125.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+from bs4 import BeautifulSoup
+import requests
+
+AMAZON = "amazon.com"
+# static copy
+TIM_BLOG = ('https://bites-data.s3.us-east-2.amazonaws.com/'
+            'tribe-mentors-books.html')
+MIN_COUNT = 3
+
+
+def load_page():
+    """Download the blog html and return its decoded content"""
+    with requests.Session() as session:
+        return session.get(TIM_BLOG).content.decode('utf-8')
+
+
+def get_top_books(content=None):
+    """Make a BeautifulSoup object loading in content,
+       find all links that contain AMAZON, extract the book title
+       (stripping spacing characters), and count them.
+       Return a list of (title, count) tuples where
+       count is at least MIN_COUNT
+    """
+    if content is None:
+        content = load_page()
+    
+    soup = BeautifulSoup(content, 'html.parser')
+    links = [link.text.replace('\n', '').strip() for link in soup.find_all('a', href=True) if AMAZON in link['href']]
+    book_count = Counter(links)
+    return book_count.most_common(16)
+
+
+print(get_top_books())
